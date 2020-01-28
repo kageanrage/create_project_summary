b17a894cd8a694876ef9654ff68a6d8807512bf1
kageanrage <kevinjpickett@gmail.com>
2020-01-29 09:12:33 +1100

    tweaks

--- create_project_summary.py
+++ create_project_summary.py
@@ -2,6 +2,7 @@ import se_admin, se_general
 from config import Config
 import bs4
 import pandas as pd
+from pprint import pprint
 
 
 cfg = Config()
@@ -32,13 +33,33 @@ df = dfs[0]  # the first df in the list
 # TODO: identify which jobs are currently live
 
 live_jobs_df = df[df.Published == True]
-print(live_jobs_df.info())
-print(live_jobs_df.head())
+# print(live_jobs_df.info())
+# print(live_jobs_df.head())
 
+# TODO: OPTIONAL - export df to excel
+# live_jobs_df.to_excel('export.xlsx')
+
+# TODO: convert live_jobs_df to a dict of per-project dicts
+jobs_reindexed = live_jobs_df.set_index('Project/IO Number')
+pprint(jobs_reindexed)
+
+jobs_list = jobs_reindexed.values.tolist()
+print(jobs_list)
 # TODO: isolate the survey id for each of those, to determine download URL
-# TODO: download current results for each of those
+    # use regex from admin_scrape.py for this
+# TODO: add survey_id to dict of dicts
+# TODO: download current results for each project
 # TODO: create dir structure (todays date = root dir)
+# testing phase root dir
+root_dir = 'C:\WorkingDir\project_updates'
+date_dir_name_template = "YYYY-MM-DD Tue"
+
 # TODO: clone and rename xlsx files from template
+fname_template = 'Summary p_num survey_name.xlsx'
 # TODO: move downloaded current results files to subdirs
+# produce a list of csv files in the downloads dir
+# find each one using p_number, then move it to the right dir
 # TODO: open all files
 # TODO: refresh data on each file (if possible)
+
+
