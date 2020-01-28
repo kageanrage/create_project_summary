d006508f20a996ea95413e7201c324f109a36543
kageanrage <kevinjpickett@gmail.com>
2020-01-28 17:10:43 +1100

    added TODOs as a starting point

---
+++ create_project_summary.py
@@ -0,0 +1,8 @@
+# TODO: scrape survey admin page
+# TODO: identify which jobs are currently live
+# TODO: download current results for each of those
+# TODO: create dir structure (todays date = root dir)
+# TODO: clone and rename xlsx files from template
+# TODO: move downloaded current results files to subdirs
+# TODO: open all files
+# TODO: refresh data on each file (if possible)
